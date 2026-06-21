f03e9bd0a032bb90a43f19411477fa7ca4542982
Luis Castrillon <luis.castrillon@movinova.com>
2026-06-21 18:53:19 -0500

    Se configuran cors

--- Api/Program.cs
+++ Api/Program.cs
@@ -21,7 +21,7 @@ const string CorsPolicy = "RifaFrontend";
 
 var allowedOrigins = builder.Configuration
     .GetSection("Cors:AllowedOrigins")
-    .Get<string[]>() ?? ["http://localhost:4200"];
+    .Get<string[]>() ?? ["https://rifa-bll7.onrender.com"];
 
 builder.Services.AddCors(options =>
 {
